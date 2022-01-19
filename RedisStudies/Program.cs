32c1f1dac090803d33c39a4d41a16ca5b9fd1044
Matheus Reis <reis.mmatheus@gmail.com>
2022-01-19 18:29:35 -0300

    Adding Consumer in Queue Example

---
+++ RedisStudies/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RedisStudies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Queue Example");
+
+            var connString = "localhost";
+            var queueKey = "github-example";
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RedisStudies/Queue/Consumer.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+
+namespace RedisStudies.Queue
+{
+    public class Consumer
+    {
+        private readonly string _connString;
+        private readonly string _queueKey;
+        public Consumer(string connString, string queueKey)
+        {
+            _connString = connString;
+            _queueKey = queueKey;
+        }
+
+        public void Pop()
+        {
+            var conn = ConnectionMultiplexer.Connect(_connString);
+
+            var database = conn.GetDatabase();
+
+            while (database.ListLength(_queueKey) > 0)
+            {
+                var data = database.ListRightPop(_queueKey);
+
+                Console.WriteLine($"{DateTime.Now:o} - {data}");
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
